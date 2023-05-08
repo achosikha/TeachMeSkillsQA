6db976bdbc121cd7efe50ad51bf6b953f455ac71
achosikha <40418629+achosikha@users.noreply.github.com>
2023-05-08 22:54:33 +0400

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,102 +1,89 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // Создать метод, который получает данные человека и их выводит
-        getPersonalData();
+        boolean isAlive = true;
 
-        // Создать метод, который получает символ и выводит сам сивол вместе с его чичловым значением
-        //charToInt();
-
-        // Написать метод с вычислением BMI
-        // getBMI();
-    }
-
-    public static void getPersonalData()
-    {
-        Scanner cout = new Scanner(System.in);
-
-        System.out.print("Please, enter your name: ");
-        String name = cout.nextLine();
-
-        System.out.print("Please, enter your surname: ");
-        String surname = cout.nextLine();
-
-        System.out.print("Please, enter your age: ");
-        int age = cout.nextInt();
-
-        System.out.print("Please, enter your height: ");
-        int height = cout.nextInt();
-
-        System.out.print("Please, enter your weight: ");
-        float weight = cout.nextFloat();
-
+        voidMethod(-1, 168, "Archil Sikharulidze", 'B', isAlive);
         System.out.println();
+        System.out.println("Your BMI equals to: " + calculateBMI(1.71, 88.8));
+        System.out.println();
+        getInitialMsg();
 
-        System.out.println("Your name is: " + name);
-        System.out.println("Your surname is: " + surname);
-        System.out.println("Your age is: " + age);
-        System.out.println("Your height is: " + height);
-        System.out.println("Your weight is: " + weight);
+        // двойное вложение метода норм, дальше лучше не надо
+        int calculateHard = getNumOne() + getNumTwo();
+        System.out.println(calculateHard);
     }
 
-    public static void charToInt() throws IOException {
-        Scanner cout = new Scanner(System.in);
-
-        System.out.print("Please enter a symbol: ");
-        char symbol = (char) cout.nextInt(); // char - 0 - 255 (ASCII), int 67 -> (char)
-
-        System.out.println("You have entered a symbol: " + symbol);
-        System.out.println("This symbols numeric meaning is: " + (int) symbol);
-
-        // int oneNumber = System.in.read(); // one element per buffer
-
-        // int oneSymbol = cout.nextLine().charAt(0);
-        // String - Archil - char 'A' 'r' 'c' 'h' 'i' 'l' (array - index[0])
-    }
-
-    public static void getBMI()
+    public static void voidMethod(int param, int paramTwo, String name, char symbol, boolean isAlive)
     {
-        Scanner cout = new Scanner(System.in);
+        // get parameters
+        double sum = param * paramTwo;
 
-        // enter height
-        System.out.print("Please, enter your height: ");
-        double height = cout.nextDouble();
-        height = (height * height) / 10000;
+        System.out.println("param * paramTwo: " + sum);
+        System.out.println("You have passed String parameter: " + name);
+        System.out.println("You have passed a char symbol: " + symbol);
+        System.out.println("Are your dreams alive?: " + isAlive );
 
-        // enter weight
-        System.out.print("Please, enter your weight: ");
-        double weight = cout.nextDouble();
-
-        // BMI = weight / (height*height)
-        double BMI = weight / height;
-
-        System.out.println("Your BMI is: " + BMI);
-        cout.close();
+        // void - что метод ничего не возвращает
     }
 
-    public static void parseVariables()
+    public static double calculateBMI(double height, double weight)
     {
-        // неявное преобразование
-        // от малого к высокому - from byte to double (long)
+        // BMI = weight / height**
+        double BMI = weight / (height * height);
 
-        // преобразование путем понижение ранка переменной - когда от double (long) к byte
-        // cast - (_date_type) _variable : (char) number;
+        return BMI;
+    }
 
-        // Несовместимые типы данных
-        String number = "1221";
+    public static int getNumOne()
+    {
+        return 10;
+    }
 
-        // No casting & no hidden transformation
-        // int intNum = (int) number; // error
-        // String numNum = (String) intNum; // error
+    public static int getNumTwo()
+    {
+        return 30;
+    }
 
-        // Parsing
-        int intNum = Integer.parseInt(number); // from String to Integer
-        // Integer -> class, method parseInt(String _var)
+    public static void getInitialMsg()
+    {
+        String msg = initialMsg();
 
-        String getValueOf = String.valueOf(intNum); // from int to String
+        System.out.println(msg);
+    }
+
+    public static String initialMsg()
+    {
+        return "Welcome to Java dear friends...";
+    }
+
+    // Information about Stack (primitive) & Heap (ссылочный)
+    public static void stackHeap()
+    {
+        // Примитивные и ссылочные типы данных
+
+        // byte, short, int, long, char, float, double, boolean -> примитивные типы данных
+
+        // String -> ссылочный
+
+        // Stack (Стэк) - примитивные типы данных, краткосрочная память, память уровня методы
+        // Heap (Куча) - ссылочные типы данных, долгосрочная память, память уровня класс, static
+
+        int age = 34; // RAM -> Stack (age -> 34)
+        int age2 = 34; // age == age2 -> легитимно, мы сравниваем два значения
+
+        String name = "Archil"; // RAM -> Stack -> address 0x5FG -> Heap -> "Archil"
+        String name2 = "Archil";
+
+        // name == name2 -> mistake, сравнит не значения, а ссылки в Стеке
+        // а ссылки типа String зачастую всегда разные
+        // == -> нельзя сравнивать ссылочные типы данных
+
+        // С++
+        // RAM -> stack - какая именно память мне нужна и туда вложить
+        // вложу в ту часть памяти, которая используется - она рухнет
     }
 }
